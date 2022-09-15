0d288b7598b98df594da4334e7fd7c38bf5fbb55
Mike Lloyd <mike.lloyd@jax.org>
2022-09-15 11:02:57 -0400

    update to docker required for web instance

---
+++ entrypoint.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+echo "mkdir $MNT_DIR"
+mkdir -p $MNT_DIR
+
+chown shiny:shiny $MNT_DIR
+ls -ld $MNT_DIR
+
+rm -rf /srv/shiny-server/*
+touch /srv/shiny-server/index.html
+ln -s $MNT_DIR /srv/shiny-server/`basename $MNT_DIR`
+
+echo "Mount $BUCKET at $MNT_DIR"
+mount.gcsfuse -o uid=999,gid=999,implicit_dirs,allow_other $BUCKET $MNT_DIR
+echo "Done"
+
+ln -s /srv/shiny-server/ /opt
+
+echo "Start shiny-server"
+sudo -u shiny shiny-server &
+
+wait -n
